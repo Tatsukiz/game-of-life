8f2730d82375aa472bfe86cfdd7571eda1f95c02
Tatsukiz <tatsukix0206@gmail.com>
2019-11-19 02:34:55 +0900

    [add]盤面作成メソッドを作成

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -3,4 +3,9 @@ public class GameOfLife {
         System.out.println("Let's Start Game Of Life!");
     }
 
+    // 盤面を作成
+    public static Board createBoard(int width, int height) {
+        return new Board(width, height);
+    }
+
 }
--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -3,4 +3,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class GameOfLifeTest {
 
+    @Test
+    void _縦5横5のボードが作成されること() {
+        Board board = GameOfLife.createBoard(5, 5);
+        assertEquals(board.getWidth(), Integer.valueOf(5));
+        assertEquals(board.getHeight(), Integer.valueOf(5));
+    }
+
 }
